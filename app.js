5ca93c0822cca43aaafa517b9584d37518f6500b
Wojciech Dudek <wdudek82@gmail.com>
2019-02-05 21:24:15 +0100

    Sending GET and POST requests, redirecting, resending data in response

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const { url, method } = req;
+
+  res.setHeader('Content-Type', 'text/html');
+
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('hello.txt', 'Hello from the Browser!:)');
+    res.writeHead(302, {
+      Location: '/foo',
+    });
+    return res.end();
+  }
+
+  res.write('<html>');
+  res.write('<head><title>Hello!</title></head>');
+  res.write('<body><h1>Hello, World!</h1></form></body>');
+  res.write('</html>');
+  return res.end();
 });
 
 server.listen(3000);
