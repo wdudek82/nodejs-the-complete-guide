57f7ddad8ba03ff3fca32db3290cc607a8f6a5ea
Wojciech Dudek <wdudek82@gmail.com>
2019-03-05 09:18:00 +0100

    Added fix for bodyParser deprecation message

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
